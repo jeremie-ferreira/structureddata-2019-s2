8dbd36d8fdcca14a836d45a7fd9333c1f47e495c
Rajan Lad <rajanrlad71293@gmail.com>
2019-06-14 01:28:47 +0200

    (BETA : May need Correction)
    Output in the console each line in the following format:
    
    Line 1: header1: value1 header2: value2 ... headerN: valueN Line 2:
    header1: value1 ... ...
    
    Then try and output it as a json object: [
    {"header1":"value1","header2":"value2",...,"headerN":"valueN"}, ...
    {"header1":"value1","header2":"value2",...,"headerN":"valueN"} ]

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Main {
 
@@ -19,16 +23,39 @@ public class Main {
 			//Get file from project resources as stream
 			InputStream inputStream = ClassLoader.class.getResourceAsStream(FILENAME);
 			//Create a buffered reader to read it line by line
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+//			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+			Scanner sc = new Scanner(new InputStreamReader(inputStream, "UTF-8"));
 			
 			//Handling the headers
-			String line = reader.readLine();
-			
+			String line = sc.nextLine();
 			String[] headers  = line.split(DELIMITER);
 			
 			for (String header : headers) {
 				System.out.print(header + " | ");
 			}
+			
+			//to transform CSV to JSON
+			JSONObject obj = new JSONObject();
+			JSONArray jArray = new JSONArray();
+			
+			
+			//Handling the values
+			while(sc.hasNext())
+			{
+				
+				String lineValues = sc.nextLine();
+				String[] values  = lineValues.split(DELIMITER);
+				
+				for (int i=0;i<values.length;i++) {
+					  obj.put(headers[i], values[i]);
+				}
+				//this are just test displays
+				System.out.print(obj);
+				jArray.add(obj);
+				System.out.println("\n");
+			}
+			//this are test displays
+			System.out.println(jArray);
 			
 			//Handling the data
 			//TODO implement this section
